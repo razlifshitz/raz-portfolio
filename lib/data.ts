2a34c174ba6abaa09f64d6ae8eb0d02a27c6a9a7
razlifshitz <razrezl@gmail.com>
2025-03-24 04:28:39 +0200

    remove rendundant file

--- lib/data.ts
+++
@@ -1,5 +0,0 @@
-import type { BlogPost } from "./blog-types"
-import { post as dynamicMenuPost } from "./blog-posts/dynamic-menu-with-silent-cache"
-
-export const blogPosts: BlogPost[] = [dynamicMenuPost]
-
